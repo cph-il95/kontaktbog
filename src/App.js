7df3c3122b8a2c77725e7649e37c86f05e202660
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-09-21 12:51:16 +0200

    ida, ida2, ida3

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import './App.css';
 import Example from './components/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Modal.css';
+import { Contact } from './entities/Contact';
 
 
 function App() {
+  const contacts = [
+    new Contact('Ida', 'Ligaard', 'blabla', '12345678', 'CphBusiness', 'Student'),
+    new Contact('Ida2', 'Ligaard', 'blabla', '12345678', 'CphBusiness', 'Student'),
+    new Contact('Ida3', 'Ligaard', 'blabla', '12345678', 'CphBusiness', 'Student'),
+    
+  ]
+  console.log(contacts);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+
+
 
       </header>
       <Example className="Example" />
+
+      {contacts.map( (contact) => {
+          return <div>{contact.firstname}</div>
+        }
+        )}
     </div>
   );
 }
